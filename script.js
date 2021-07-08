cd9f9fdbd5c9704cc221d65971abfbea1411526b
Bharamiyati <bharamiyati@gmail.com>
2021-07-08 18:40:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const kontak = document.getElementById('kontak');
+kontak.textContent = "kontak saya";
+console.log(kontak.textContent);
+
+
+function mOver(obj) {
+  obj.innerHTML = "Saya, Bharamiyati Athalas Bharka"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Hallo, Selamat Datang"
+}
